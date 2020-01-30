39940c27084c9214b155d150b851df98385c42c6
Ricky Chow <rickychow@google.com>
2020-01-31 08:55:17 +1100

    Added layerstack option

--- server/src/main/java/com/genymobile/scrcpy/Server.java
+++ server/src/main/java/com/genymobile/scrcpy/Server.java
@@ -31,7 +31,7 @@ public final class Server {
 
             try {
                 // synchronous
-                screenEncoder.streamScreen(device, connection.getVideoFd());
+                screenEncoder.streamScreen(device, connection.getVideoFd(), options.getLayerStack());
             } catch (IOException e) {
                 // this is expected on close
                 Ln.d("Screen streaming stopped");
@@ -79,7 +79,7 @@ public final class Server {
                     "The server version (" + clientVersion + ") does not match the client " + "(" + BuildConfig.VERSION_NAME + ")");
         }
 
-        if (args.length != 8) {
+        if (args.length != 9) {
             throw new IllegalArgumentException("Expecting 8 parameters");
         }
 
@@ -106,6 +106,9 @@ public final class Server {
 
         boolean control = Boolean.parseBoolean(args[7]);
         options.setControl(control);
+
+        int layerStack = Integer.parseInt(args[8]);
+        options.setLayerStack(layerStack);
 
         return options;
     }
